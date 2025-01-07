5ffcbc526847661b9e4cf73257c30645f8c49c49
Francesco Barone <francescobarone@Mac.homenet.telecomitalia.it>
2025-01-07 13:54:07 +0100

    Nwe S

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -5,18 +5,25 @@ import Loader from '../Loader/Loader';
 import logo from '../Images/chatrathLogo.png';
 import hidev from "../Images/hi-dev.png";
 import bg2 from "../Images/bg3.mp4";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import { Mousewheel, Pagination, Autoplay, Navigation } from 'swiper/modules';
 
 function LandingPage() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [isScrolled, setIsScrolled] = useState(false);
-    const cursorRef = useRef(null);
 
     const menuRef = useRef(null);
     const closeButtonRef = useRef(null);
 
     const nickSectionRef = useRef(null);
 
+
+    const [visibleIndex, setVisibleIndex] = useState(-1);
+    const stepsRef = useRef([]);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -32,17 +39,17 @@ function LandingPage() {
         }
     };
 
-    const handleMouseMove = (event) => {
-        if (cursorRef.current) {
-            cursorRef.current.style.left = `${event.clientX}px`;
-            cursorRef.current.style.top = `${event.clientY}px`;
-        }
-    };
+    
 
     const handleScroll = () => {
-        setIsScrolled(window.scrollY > 50);
+        const scrollPosition = window.scrollY + window.innerHeight;
+        stepsRef.current.forEach((step, index) => {
+            if (step && step.getBoundingClientRect().top < scrollPosition) {
+                setVisibleIndex(index);
+            }
+        });
     };
-    
+
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
         return () => {
@@ -50,13 +57,7 @@ function LandingPage() {
         };
     }, []);
 
-    useEffect(() => {
-        document.addEventListener("mousemove", handleMouseMove);
-
-        return () => {
-            document.removeEventListener("mousemove", handleMouseMove);
-        };
-    }, []);
+    
 
     useEffect(() => {
         if (isMenuOpen) {
@@ -109,17 +110,90 @@ function LandingPage() {
         },
     ];
 
+    const stepsData = [
+        {
+            icon: "fas fa-star",
+            title: "Step 1: Introduction",
+            description: "This is the description for the first step."
+        },
+        {
+            icon: "fas fa-rocket",
+            title: "Step 2: Analysis",
+            description: "This is the description for the second step."
+        },
+        {
+            icon: "fas fa-brain",
+            title: "Step 3: Development",
+            description: "This is the description for the third step."
+        },
+        {
+            icon: "fas fa-cogs",
+            title: "Step 4: Implementation",
+            description: "This is the description for the fourth step."
+        },
+        {
+            icon: "fas fa-check",
+            title: "Step 5: Feedback",
+            description: "This is the description for the fifth step."
+        },
+    ];
+
+    const testimonialsData = [
+        {
+            icon: "fas fa-user",
+            name: "John Doe",
+            profession: "Software Engineer",
+            description: "This service has changed my life for the better!"
+        },
+        {
+            icon: "fas fa-user",
+            name: "Jane Smith",
+            profession: "Product Manager",
+            description: "An amazing experience, highly recommend!"
+        },
+        {
+            icon: "fas fa-user",
+            name: "Alice Johnson",
+            profession: "UX Designer",
+            description: "I learned so much and grew professionally."
+        },
+        {
+            icon: "fas fa-user",
+            name: "Bob Brown",
+            profession: "Data Scientist",
+            description: "A fantastic program that delivers results."
+        },
+        {
+            icon: "fas fa-user",
+            name: "Charlie Green",
+            profession: "Marketing Specialist",
+            description: "Incredible insights and support throughout."
+        },
+        {
+            icon: "fas fa-user",
+            name: "Diana White",
+            profession: "Business Analyst",
+            description: "I can't recommend this enough!"
+        },
+    ];
+
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
+
     return (
         <>
-            <div ref={cursorRef} className="custom-cursor"></div>
             {isLoading ? (
                 <Loader />
             ) : (
                 <div className={`landing-container ${!isLoading ? 'landing-appear' : ''}`}>
-                    <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+                    <nav className="navbar">
                         <img src={logo} alt="Logo" className="logo" />
                         <ul>
-                            <li><a className="ai-assistant" href="#ai-assistant">AI Assistant</a></li>
+                            <li><a className="ai-assistant" href="/login">AI Assistant</a></li>
                             <li><a className="contact-me" href="#contact-me">Contact Me</a></li>
                             <li className="menu-hamburger">
                                 {isMenuOpen ? (
@@ -140,9 +214,9 @@ function LandingPage() {
                     </nav>
                     <div ref={menuRef} className={isMenuOpen ? 'menu-appear view' : 'menu-appear hidden'}>
                         <p className='menu-item' onClick={() => moveTo('coaching')}>1. Why Coaching</p>
-                        <p className='menu-item' onClick={() => moveTo('nick')}>2. Why Nick</p> {/* Aggiunto il click per "Why Nick" */}
-                        <p className='menu-item'>3. How AI coaching works</p>
-                        <p className='menu-item'>4. Testimonials</p>
+                        <p className='menu-item' onClick={() => moveTo('nick')}>2. Why Nick</p> 
+                        <p className='menu-item' onClick={() => moveTo('how-ai-coaching-works')}>3. How AI coaching works</p>
+                        <p className='menu-item' onClick={() => moveTo('testimonials')}>4. Testimonials</p>
                         <p className='menu-item'>5. Selecting the best coach</p>
                         <p className='menu-item'>6. Understanding The Threshold</p>
                     </div>
@@ -199,6 +273,69 @@ function LandingPage() {
                                 Descrizione aggiuntiva qui se necessaria
                             </p>
                         </div>
+                    </div>
+
+                    <div className="how-ai-coaching-works" id='how-ai-coaching-works'>
+                        <h2 className="coaching-title">How AI Coaching Works</h2>
+                        <Swiper
+                            direction={'vertical'}
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            mousewheel={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            modules={[Mousewheel, Pagination]}
+                            className="mySwiper"
+                        >
+                            {stepsData.map((step, index) => (
+                                <SwiperSlide key={index}>
+                                    <div className='step-item-content'>
+                                        <i className={step.icon}></i>
+                                        <h3>{step.title}</h3>
+                                        <p>{step.description}</p>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+
+                    <div className="testimonials-section" id='testimonials'>
+                        <h2 className="coaching-title">Testimonials</h2>
+                        <Swiper
+                            spaceBetween={30}
+                            centeredSlides={true}
+                            autoplay={{
+                              delay: 2500,
+                              disableOnInteraction: false,
+                            }}
+                            pagination={{
+                              clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Autoplay, Pagination, Navigation]}
+                            onAutoplayTimeLeft={onAutoplayTimeLeft}
+                            className="myTestimonialsSwiper"
+                        >
+                            {testimonialsData.map((testimonial, index) => (
+                                <SwiperSlide key={index} className='testimonial-ss'>
+                                    <div className="testimonial-card">
+                                        <div className="testimonial-icon">
+                                            <i className={testimonial.icon}></i>
+                                        </div>
+                                        <h3>{testimonial.name}</h3>
+                                        <p className="profession">{testimonial.profession}</p>
+                                        <p className="testimonial-description">"{testimonial.description}"</p>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                            <div className="autoplay-progress" slot="container-end">
+                                <svg viewBox="0 0 48 48" ref={progressCircle}>
+                                    <circle cx="24" cy="24" r="20"></circle>
+                                </svg>
+                                <span ref={progressContent}></span>
+                            </div>
+                        </Swiper>
                     </div>
                 </div>
             )}
